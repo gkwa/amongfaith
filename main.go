66e63303d9e79f9ee8d119305fc110b8d62cd838
Your Name <you@example.com>
2022-03-03 15:01:06 -0800

    service

--- main.go
+++ main.go
@@ -1,6 +1,68 @@
-// main.go
 package main
 
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/kardianos/service"
+)
+
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	fname := "C:\\test.txt"
+
+	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+	text := "hellowrold ......... "
+	counter := 0
+	strint := ""
+
+	for true {
+		strint = strconv.Itoa(counter)
+		if _, err = f.WriteString(text + strint + "\n"); err != nil {
+			panic(err)
+		}
+		counter++
+		time.Sleep(1 * time.Second)
+	}
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
 func main() {
-  println("Ba dum, tss!")
+	svcConfig := &service.Config{
+		Name:        "GoServiceExampleSimple",
+		DisplayName: "Go Service Example",
+		Description: "This is an example Go service.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
